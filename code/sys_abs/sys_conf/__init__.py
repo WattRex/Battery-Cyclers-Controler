caafcb76c6cc11c71519a4e4dfd3a9857436e323
RobertoAldea <r.aldea.csic@gmail.com>
2023-07-12 09:05:44 +0100

    Added comments for modules

--- code/sys_abs/sys_conf/__init__.py
+++ code/sys_abs/sys_conf/__init__.py
@@ -1,5 +1,6 @@
 """
 This file specifies what is going to be exported from this module.
+In this case is sys_conf
 """
 from .sys_conf import sys_conf_read_config_params,\
     sys_conf_get_argv_password, SysConfSectionNotFoundErrorC
--- code/sys_abs/sys_log/__init__.py
+++ code/sys_abs/sys_log/__init__.py
@@ -1,4 +1,6 @@
-"""AI is creating summary for
+"""
+This file specifies what is going to be exported from this module.
+In this case is sys_log.
 """
 from .sys_log import SysLogLoggerC, sys_log_logger_get_module_logger, SysLogCustomFormatterC
 
