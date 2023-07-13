2a00fd537af8670f502c545630d2ae41473e24e9
marius <marius.crisan@csic.es>
2023-07-13 09:04:52 +0100

    Develop drv_db and define header imports order

---
+++ code/drv/drv_db/drv_db_types.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+'''
+Enumerations defined to standardize names.
+'''
+#######################        MANDATORY IMPORTS         #######################
+
+
+#######################         GENERIC IMPORTS          #######################
+
+
+#######################       THIRD PARTY IMPORTS        #######################
+from sqlalchemy import Enum
+
+#######################      LOGGING CONFIGURATION       #######################
+from sys_abs.sys_log import sys_log_logger_get_module_logger
+log = sys_log_logger_get_module_logger(__name__)
+
+
+#######################          PROJECT IMPORTS         #######################
+
+
+#######################          MODULE IMPORTS          #######################
+
+
+#######################              ENUMS               #######################
+
+### CYCLER ###
+class DrvDbBatteryTechE(Enum):
+    '''Enum used to define battery technology types.
+    '''
+    LITHIUM = 'Lithium'
+    LEADACID = 'LeadAcid'
+    REDOXSTACK = 'RedoxStack'
+
+
+class DrvDbLithiumChemistryE(Enum):
+    '''Enum used to define lithium battery chemistry.
+    '''
+    NMC = 'NMC'
+    NCA = 'NCA'
+    LMO = 'LMO'
+    LFP = 'LFP'
+    LCO = 'LCO'
+    OTHER = 'Other'
+
+
+class DrvDbLeadAcidChemistryE(Enum):
+    '''Enum used to define leadacid battery chemistry.
+    '''
+    LIQUID = 'Liquid'
+    GEL = 'GEL'
+    OTHER = 'Other'
+
+
+class DrvDbBipolarTypeE(Enum):
+    '''Enum used to define redox bipolar type.
+    '''
+    PAPYEX_MERSEN = 'Papyex Mersen'
+    COMPOSITE_SCHUNK = 'Composite Schunk'
+    GRAPHITE = 'Graphite'
+    OTHER = 'Other'
+
+
+class DrvDbMembraneTypeE(Enum):
+    '''Enum used to define redox membrane type.
+    '''
+    FUMASEP_ANIONIC = 'Fumasep-Anionic'
+    FUMASEP_CATHIONIC = 'Fumasep-Cathionic'
+    NAFION = 'Nafion'
+    VANADION = 'Vanadion'
+    PEEK = 'PEEK'
+    OTHER = 'Other'
+
+
+class DrvDbElectrolyteTypeE(Enum):
+    '''Enum used to define redox electrolyte type.
+    '''
+    ALL_VANADIUM = 'All-vanadium'
+    ALL_IRON = 'All-iron'
+    VANADIUM_BASED = 'Vanadium-based'
+    IRON_BASED = 'Iron-based'
+    OTHER = 'Other'
+
+
+class DrvDbDeviceTypeE(Enum):
+    '''Enum used to define compatible device type.
+    '''
+    SOURCE = 'Source'
+    BISOURCE = 'BiSource'
+    LOAD = 'Load'
+    METER = 'Meter'
+
+
+class DrvDbEquipStatusE(Enum):
+    '''Enum used to define equipment working status.
+    '''
+    COMM_ERROR = 'COMM'
+    OK = 'OK'
+    INTER_ERROR = 'INTER'
+
+
+class DrvDbExpStatusE(Enum):
+    '''Enum used to define experiment status types.
+    '''
+    ERROR = 'ERROR'
+    FINISHED = 'FINISHED'
+    PAUSED = 'PAUSED'
+    RUNNING = 'RUNNING'
+    QUEUED = 'QUEUED'
+
+
+class DrvDbCyclingModeE(Enum):
+    '''Enum used to define cycling control modes.
+    '''
+    WAIT = 'WAIT'
+    CC = 'CC'
+    CV = 'CV'
+    CP = 'CP'
+
+
+class DrvDbCyclingLimitE(Enum):
+    '''Enum used to define cycling limits.
+    '''
+    TIME = 'TIME'
+    VOLTAGE = 'VOLTAGE'
+    CURRENT = 'CURRENT'
+    POWER = 'POWER'
+
+
+#######################             CLASSES              #######################
+
+
+#######################            FUNCTIONS             #######################
